b8e0bec1d1eba0e47aab428790286ddd92f297b5
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-10-07 21:26:45 +0300

    Basic things for starting.

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Murat Ceyhun Korpeoglu on 7.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
