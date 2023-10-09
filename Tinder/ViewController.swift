6e382d1ec9df8eac11b77dc6dbaa8c98eca8b9a6
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-10-09 22:19:31 +0300

    Building UI easily with StackViews

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -12,10 +12,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
         
-        view.backgroundColor = .cyan
+        
+        let subviews = [UIColor.gray, .darkGray, .black].map { color in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subviews)
+        topStackView.distribution = .fillEqually
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+        stackView.axis = .vertical
+//        stackView.frame = .init(x: 0, y: 0, width: 300, height: 300)
+        view.addSubview(stackView)
+        stackView.fillSuperview()
+//        stackView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+        
+        
     }
-
-
 }
 
