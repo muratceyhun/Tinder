74bd6557e943416ea8bafbf82c7277856de9f68d
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-10-11 23:36:41 +0300

    CardViews and UIPanGesture

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -10,24 +10,35 @@ import UIKit
 class ViewController: UIViewController {
     
     let topStackView = TopNavigationStackView()
-    let blueView = UIView()
+    let cardsDeckView = UIView()
     let buttonsStackView = HomeBottomControlsStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-        blueView.backgroundColor = .blue
         setupLayout()
+        setupDummyCards()
     }
     
+    
+    fileprivate func setupDummyCards() {
+        
+        let cardView = CardView()
+        cardsDeckView.addSubview(cardView)
+        cardView.fillSuperview()
+    }
+        
     //MARK: -Fileprivate
     
     fileprivate func setupLayout() {
-        let overallstackView = UIStackView(arrangedSubviews: [topStackView, blueView, buttonsStackView])
+        let overallstackView = UIStackView(arrangedSubviews: [topStackView, cardsDeckView, buttonsStackView])
         overallstackView.axis = .vertical
         view.addSubview(overallstackView)
         overallstackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+        overallstackView.isLayoutMarginsRelativeArrangement = true
+        overallstackView.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
+        overallstackView.bringSubviewToFront(cardsDeckView)
     }
 }
 
