96f4d8c60deb0cfab65e4356dc14e946fddf8b30
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-10-17 18:05:13 +0300

    Intro MVVM CardViewModels

--- Tinder/Model/User .swift
+++ Tinder/Model/User .swift
@@ -5,7 +5,7 @@
 //  Created by Murat Ceyhun Korpeoglu on 16.10.2023.
 //
 
-import Foundation
+import UIKit
 
 
 struct User {
@@ -14,5 +14,15 @@ struct User {
     let age: Int
     let profession: String
     let imageName: String
+    
+    func toCardViewModel() -> CardViewModel {
+        
+        let attributedText = NSMutableAttributedString(string: name, attributes: [.font : UIFont.systemFont(ofSize: 32, weight: .heavy)])
+        attributedText.append(NSAttributedString(string: "  \(age)", attributes: [.font : UIFont.systemFont(ofSize: 24, weight: .regular)]))
+        attributedText.append(NSAttributedString(string: "\n\(profession)", attributes: [.font : UIFont.systemFont(ofSize: 20, weight: .regular)]))
+        
+        
+        return CardViewModel(imageName: imageName, attributedString: attributedText, textAlignment: .left)
+    }
     
 }
---
+++ Tinder/ViewModels/CardViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewModel.swift
+//  Tinder
+//
+//  Created by Murat Ceyhun Korpeoglu on 17.10.2023.
+//
+
+import UIKit
+
+
+struct CardViewModel {
+    
+    let imageName: String
+    let attributedString: NSAttributedString
+    let textAlignment: NSTextAlignment
+    
+}
